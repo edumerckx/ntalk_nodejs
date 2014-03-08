50f346d12c3f14e0bb8e2e2b40aa9247306c9dec
edumerckx <edu_merckx@yahoo.com.br>
2014-03-08 18:47:45 -0300

    Socket.io

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ var express = require('express');
 var load = require('express-load');
 var app = express();
 var error = require('./middleware/error');
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+const KEY = 'ntalk.sid', SECRET = 'ntalk';
+var cookie = express.cookieParser(SECRET)
+	, store = new express.session.MemoryStore()
+	, sessOpts = {secret: SECRET, key: KEY, store: store}
+	, session = express.session(sessOpts);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.cookieParser('ntalk'));
-app.use(express.session());
+app.use(cookie);
+app.use(session);
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
@@ -15,11 +23,29 @@ app.use(express.static(__dirname + '/public'));
 app.use(error.notFound);
 app.use(error.serverError);
 
+// autorização socket.io
+io.set('authorization', function(data, accept) {
+	cookie(data, {}, function(err) {
+		var sessionID = data.signedCookies[KEY];
+		store.get(sessionID, function(err, session) {
+			if (err || !session) {
+				accept(null, false);
+			} else {
+				data.session = session;
+				accept(null, true);
+			}
+		});
+	});
+});
+
+
 load('models')
 	.then('controllers')
 	.then('routes')
 	.into(app);
+load('sockets')
+	.into(io);
 
-app.listen(3000, function(){
+server.listen(3000, function(){
 	console.log("Ntalk no ar.");
 });
---
+++ sockets/chat.js
@@ -0,0 +1,35 @@
+module.exports = function(io) {
+	var crypto = require('crypto');
+	var sockets = io.sockets;
+	sockets.on('connection', function(client) {
+		var session = client.handshake.session;
+		var usuario = session.usuario;	
+		// send-server - quando o usuário envia mensagens
+		client.on('send-server', function(msg) {
+			var msg = "<b>" + usuario.nome + ":</b> " + msg + "<br>";
+			client.get('sala', function(erro, sala) {
+				var data = {email: usuario.email, sala: sala};
+				client.broadcast.emit('new-message', data);
+				sockets.in(sala).emit('send-client', msg);
+			});
+		});
+		// join - quando o usuário entra na sala
+		client.on('join', function(sala) {
+			if (sala) {
+				sala = sala.replace('?', '');
+			} else {
+				var timestamp = new Date().toString();
+				var md5 = crypto.createHash('md5');
+				sala = md5.update(timestamp).digest('hex');
+			}
+			client.set('sala', sala);
+			client.join(sala);
+		});
+		// disconnect - quando o usuário sai da sala
+		client.on('disconnect', function() {
+			client.get('sala', function(erro, sala) {
+				client.leave(sala);
+			});
+		});
+	});
+}
